6ba93c32dbb710b213c776f45b0110babc228caf
Seth <sethc.dorris@gmail.com>
2015-07-28 11:18:11 -0500

    more work

--- main.js
+++ main.js
@@ -1,49 +1,14 @@
-var songs = [];
-var artist = [];
-var album = [];
-
-songs[songs.length] = "Legs";
-songs[songs.length] = "The Logical Song";
-songs[songs.length] = "Another Brick in the Wall";
-songs[songs.length] = "Welco(me to the Jungle";
-songs[songs.length] = "Ironi!c";
-songs[songs.length] = "Burn"
-songs[songs.length] = "Burn"
-
-artist[artist.length] = "ZZTop";
-artist[artist.length] = "Supertramp";
-artist[artist.length] = "Pink Floyd";
-artist[artist.length] = "Guns & Roses";
-artist[artist.length] = "Alanis Morisette";
-artist[artist.length] = "In This Moment";
-artist[artist.length] = "Mad at Gravity";
-
-album[album.length] = "Eliminator";
-album[album.length] = "Breakfast in America";
-album[album.length] = "The Wall";
-album[album.length] = "Appetite for Destruction";
-album[album.length] = "Jagged Little Pill";
-album[album.length] = "Blood";
-album[album.length] = "Resonance";
+$(document).ready(function() {
+	$.ajax({
+		url: "songs.JSON"
+	}).done(function(data) {
+		console.log(data);
+		for (var i = 0; i < data.songs.length; i++) {
+			$('#main-content').append("<div class='content-song'>" + data.songs[i] + "</div>");
+			$('#main-content').append("<div class='content-artist'>" + "<em>by:</em> "+ data.artist[i] + "<span class='content-album'>" 
+				+ "<em>off the album: </em>" + data.album[i] + "</span>" + "</div>");
+		}
+	})
+});
 
 
-var mainContent = document.getElementById("main-content");
-
-for (var i = 0; i < songs.length; i++) {
-	songs[i] = songs[i].replace(/\>/g, "-");
-	songs[i] = songs[i].replace(/!/g, "");
-	songs[i] = songs[i].replace(/@/g, "");
-	songs[i] = songs[i].replace(/#/g, "");
-	songs[i] = songs[i].replace(/$/g, "");
-	songs[i] = songs[i].replace(/%/g, "");
-	songs[i] = songs[i].replace(/^/g, "");
-	songs[i] = songs[i].replace(/&/g, "");
-	songs[i] = songs[i].replace(/\*/g, "");
-	songs[i] = songs[i].replace(/\(/g, "");
-	songs[i] = songs[i].replace(/\)/g, "");	
-
-    mainContent.innerHTML += "<div>";
-	mainContent.innerHTML += "<h4 class=\"first-song-name\">" + songs[i] + "</h4>";
-	mainContent.innerHTML += "<span class=\"info-spacer\">" + "By: " +artist[i] + "</span>" + "<span class=\"info-spacer\">" + album[i] + "</span>" + "<span class=\"genre\">" + "Rock" + "</span>";
-	mainContent.innerHTML += "</div>";
-}
