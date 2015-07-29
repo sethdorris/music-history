d47b81b15030b6d2b96d6aea2c63be575e740254
Seth <sethc.dorris@gmail.com>
2015-07-29 13:35:38 -0500

    july 29

--- main.js
+++ main.js
@@ -9,6 +9,21 @@ $(document).ready(function() {
 				+ "<em>off the album: </em>" + data.album[i] + "</span>" + "</div>");
 		}
 	})
+
+
+
+	$('#more').click(function(){
+		$.ajax({
+			url: "songs2.JSON"
+		}).done(function(data) {
+			console.log(data);
+			for (var i = 0; i < data.songs.length; i++) {
+			$('#main-content').append("<div class='content-song'>" + data.songs[i] + "</div>");
+			$('#main-content').append("<div class='content-artist'>" + "<em>by:</em> "+ data.artist[i] + "<span class='content-album'>" 
+				+ "<em>off the album: </em>" + data.album[i] + "</span>" + "</div>");
+		}
+	})
+	})
 });
 
 
